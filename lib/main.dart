8d208b32180c23ba2ec1385839071c603bdc5812
taniacse <taniacse1@gmail.com>
2019-10-03 01:23:17 +0600

    bottom backround image problem solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_suriya/ui/movie_list.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(App());
 
-
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: MovieList(),
+        appBar: AppBar(),
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: SvgPicture.asset(
+                  'assets/images/loginHeader.svg',
+                  alignment: Alignment.bottomRight,
+                ),
+              ),
+
+              ListView(
+                children: <Widget>[
+                 Container(
+                   height: 100,
+                   child: Text('Bangladesh',
+                   style: TextStyle(
+                     fontSize: 30
+                   ),
+                   ),
+                 ),Container(
+                    height: 100,
+                   child: Text('Bangladesh',
+                   style: TextStyle(
+                     fontSize: 30
+                   ),
+                   ),
+                 ),Container(
+                    height: 100,
+                   child: Text('Bangladesh',
+                   style: TextStyle(
+                     fontSize: 30
+                   ),
+                   ),
+                 ),Container(
+                    height: 100,
+                   child: Text('Bangladesh',
+                   style: TextStyle(
+                     fontSize: 30
+                   ),
+                   ),
+                 ),Container(
+                    height: 100,
+                   child: Text('Bangladesh',
+                   style: TextStyle(
+                     fontSize: 30
+                   ),
+                   ),
+                 ),Container(
+                    height: 100,
+                   child: Text('Bangladesh',
+                   style: TextStyle(
+                     fontSize: 30
+                   ),
+                   ),
+                 )
+                ],
+
+              ),
+
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: null),
       ),
-
-
-
     );
   }
-
-
 }
-
-
-
